2f508c487f2636e43d0ea4c3416af134982fc629
Joe Hayden <eljoeyy@gmail.com>
2019-08-03 15:11:43 -0500

    add display controller event listeners

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const gameBoard = (() => {
     const board = ['', '', '','', '', '','', '', ''];
 //change 'x' in markBoard to player.mark after player factory set up
-    const mark = (index) => (checkMoveValid(index)) ? board[index] = 'x' : false;
+    const mark = (index, mark) => (checkMoveValid(index)) ? board[index] = 'x' : false;
     const checkMoveValid = (index) => (board[index] === '') ? true : false;
     const reset = () => {
         board.forEach((element, index) => board[index] = '');
@@ -14,3 +14,18 @@ const gameBoard = (() => {
     };
 })();
 
+const displayController = (() => {
+    const addEvList = () => {
+        let gamePad = document.querySelectorAll('.box')
+            gamePad.forEach( function(element) {
+                element.addEventListener('click', () => gameBoard.mark(player.mark));
+        });
+    };
+
+    return {
+        addEvList,
+    };
+})();
+
+
+//reminders
