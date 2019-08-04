824dde148e36c661fb5406a7454dde8ba3d50835
Joe Hayden <eljoeyy@gmail.com>
2019-08-03 19:21:30 -0500

    add reset ability

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//*Handles gameboard */
+
 const gameBoard = (() => {
     const board = ['', '', '','', '', '','', '', ''];
 //change 'x' in markBoard to player.mark after player factory set up
@@ -14,12 +16,16 @@ const gameBoard = (() => {
     };
 })();
 
+//** Player */
+
 const Player = (name, symbol) => {
     const getName = () => name;
     const getSymbol = () => symbol;
 
-    return {getName, getSymbol}
+    return {getName, getSymbol,}
 }
+
+//** Handles display */
 
 const displayController = (() => {
     const board = document.querySelectorAll('.box');
@@ -29,12 +35,13 @@ const displayController = (() => {
     const gamemodePvP = document.getElementById('PvP');
      
     const gameListeners = () => {
+        resetBTN.addEventListener('click', game.reset)
         board.forEach( function(element) {
-            element.addEventListener('click', () => gameBoard.mark((element.id - 1), playermark));
-            element.addEventListener('click', () => element.innerHTML = playermark);
+            element.addEventListener('click', () => gameBoard.mark((element.id - 1), game.currentPlayer.getSymbol()));
+            element.addEventListener('click', () => (element.innerHTML = game.currentPlayer.getSymbol()));
         });
        
-        resetBTN.addEventListener('click', game.reset())
+        
     };
     
     const getGameMode = () => {
@@ -43,16 +50,55 @@ const displayController = (() => {
         if (gamemodePvP.checked) return gamemodePvP.id
     }
 
+    const reset = () => {
+        board.forEach( function(element) {
+            element.innerHTML = '';
+        });
+    }
+
 
     return {
         gameListeners,
         getGameMode,
-
+        reset,
     };
 })();
 
-const game = () => {
-};
+const game = (() => {
+    const winningParameters = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+    const playerOne = Player('You', 'O');
+    const playerTwo = Player('AI', 'X');
+    let currentPlayer = playerOne;
+
+    const setGameMode = () => displayController.getGameMode();
+
+    const swapCurrentPlayer = () => {
+        if(currentPlayer === playerOne) {
+            currentPlayer = playerTwo;
+            console.log(currentPlayer.name)
+        } else {
+            currentPlayer = playerOne;
+            console.log(currentPlayer.name)
+        }
+    };
+    
+    const reset = () => {
+        gameBoard.reset();
+        displayController.reset();
+    }
+
+    return {
+        setGameMode,
+        playerOne,
+        swapCurrentPlayer,
+        currentPlayer,
+        reset,
+    };
+})();
 
 
 //reminders
