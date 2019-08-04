27c5922901d38ab7d09995669feeaf6b6fafbba2
Joe Hayden <eljoeyy@gmail.com>
2019-08-04 09:42:13 -0500

    add checks for computer wins and start button

--- app.js
+++ app.js
@@ -28,19 +28,19 @@ const Player = (name, symbol) => {
 
 const displayController = (() => {
     const board = document.querySelectorAll('.box');
-    const resetBTN = document.querySelector('.resetBTN')
+    const resetBTN = document.querySelector('.resetBTN');
+    const startBTN = document.querySelector('.startBTN');
     const gamemodeAI = document.getElementById('AI1');
     const gamemodeAI2 = document.getElementById('AI2');
     const gamemodePvP = document.getElementById('PvP');
      
     const gameListeners = () => {
         resetBTN.addEventListener('click', game.reset)
+        startBTN.addEventListener('click', game.start)
         
         board.forEach( function(element) {            
             element.addEventListener('click', () => game.playTurn(element));
         });
-       
-        
     };
     
     const getGameMode = () => {
@@ -66,12 +66,8 @@ const displayController = (() => {
 //** Handles Game */
 
 const game = (() => {
-    let turns = 0;
-    const winningParameters = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8],
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],
-        [0, 4, 8], [2, 4, 6]
-    ];
+    let gameOn = false;
+    let turns = 0;    
     const playerOne = Player('You', 'O');
     const playerTwo = Player('AI', 'X');
     let currentPlayer = playerOne;
@@ -80,47 +76,65 @@ const game = (() => {
     const swapCurrentPlayer = () => 
         (game.currentPlayer === playerOne) ? game.currentPlayer = playerTwo: game.currentPlayer = playerOne;
     
+    const start = () => {
+        reset();
+        gameOn = true;
+        console.log(gameOn)
+    }
+
     const reset = () => {
         gameBoard.reset();
         displayController.reset();
         turns = 0;
+        gameOn = false;
     }
 
     const playTurn = (element) => {
+        if(!gameOn) return;
+
         //Checks for valid move then marks square and swaps players
         if(gameBoard.checkMoveValid(element.id)) {
             element.innerHTML = game.currentPlayer.getSymbol();
-            gameBoard.mark((element.id), game.currentPlayer.getSymbol())
-            swapCurrentPlayer();
+            gameBoard.mark((element.id), game.currentPlayer.getSymbol())            
             turns ++;
         //Checks if theres a winner   
             if (checkForWinner()) {
                 console.log('YOU WON')
-                game.reset();
             } else if(turns == 9) {
                 console.log('YOU TIE!');                
             } else {
+        //will need computer move here
             }
-
+            swapCurrentPlayer();
         }
         
     }
 
     const checkForWinner = () => {
         let board = gameBoard.show;
-        let playerCheck = [];
-    //Loop through gameboard to get all the indexes of 'O'  
+        let playerPicks = [];
+        let computerPicks = [];
+        const winningParameters = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+        let check = function (winArr, arrCheck) {
+                return winArr.every(j => arrCheck.includes(j));
+            }
+
+    //Loop through gameboard to get all the indexes of the pieces for both players  
         for(let i = 0; i < board.length; i ++)
             if(board[i] === 'O') {
-                playerCheck.push(i);
+                playerPicks.push(i);
+            } else if(board[i] === 'X') {
+                computerPicks.push(i);
             }
-    //Loop through Win parameters to see if player won
-        for(let i = 0; i < game.winningParameters.length; i ++) {
-            let checkArray = game.winningParameters[i]
-            let check = function (checkArray, playerCheck) {
-                return checkArray.every(j => playerCheck.includes(j));
-            }
-            if (check(checkArray, playerCheck)) {
+    //Loop through Win parameters to see if theres a winner
+        for(let i = 0; i < winningParameters.length; i++) {
+            let checkArray = winningParameters[i]
+            
+            if (check(checkArray, playerPicks) || check(checkArray, computerPicks) ) {
                 return true
             } 
        }
@@ -133,11 +147,12 @@ const game = (() => {
         gameMode,
         playTurn,
         checkForWinner,
-        winningParameters,
+        start,
     };
 })();
 
+displayController.gameListeners();
 
 //reminders
-//check for winners not done got players check done. finish comp check
 //Add win and tie 
+//add computer move ability
